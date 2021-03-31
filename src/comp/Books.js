71722627ff5e69967c9dc30111a5d67175e8f90f
ahmed atta <ahmedatta3322@gmail.com>
2021-03-31 21:42:49 +0200

    get request

--- src/comp/Books.js
+++ src/comp/Books.js
@@ -1,6 +1,9 @@
 import React from 'react'
-
+import {Get} from 'react-axios'
+import { Table } from 'react-bootstrap';
 export default function Books() {
+
+
     return (
         <Get url="http://127.0.0.1:5000/getbooks">
         {(error, response, isLoading, makeRequest, axios) => {
@@ -11,7 +14,32 @@ export default function Books() {
             return (<div>Loading...</div>)
           }
           else if(response !== null) {
-            return (<div>{response.data.message} <button onClick={() => makeRequest({ params: { refresh: true } })}>Refresh</button></div>)
+            console.log(response.data.thelist)
+            //{response.data.message}response.data.thelist.map(e => {})
+            return (<div>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                  <th>#</th>
+                  <th>Title</th>
+                  <th>Authors</th>
+                  <th>Stock</th>
+                  <th>Isbn</th>
+                  </tr>
+                </thead>
+                {
+                  response.data.thelist.map(e => {
+                    return <tr>
+                    <td>{e.id}</td>
+                    <td>{e.title}</td>
+                    <td>{e.authors}</td>
+                    <td>{e.stock}</td>
+                    <td>{e.isbn}</td>
+                  </tr>
+                  })
+                }
+              </Table>
+               <button onClick={() => makeRequest({ params: { refresh: true } })}>Refresh</button></div>)
           }
           return (<div>Default message before request is made.</div>)
         }}
